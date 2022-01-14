e3b09383b6a92d4e36a9af572595bd3731eac475
CassianoCardoso <dj_kassiano@hotmail.com>
2022-01-14 20:28:17 -0300

    Save score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.devsuperior.dsmovie.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+// Vai fazer as buscas dentro do sql
+	
+	User findByEmail(String email);
+	
+}
